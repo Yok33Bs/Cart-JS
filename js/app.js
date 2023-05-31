2e28b9f86607df102a555bad844237a2d26d045f
Jorge Moya <17.jorgemoya@gmail.com>
2023-05-30 23:36:51 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,88 @@
+const listaCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarrito = document.querySelector('#vaciar-carrito');
+const agregarCarrito = document.querySelectorAll('.agregar-carrito');
+let carrito = [];
+
+cargarEventListeners();
+function cargarEventListeners(){
+    agregarCarrito.forEach(curso=>curso.addEventListener('click', agregarCurso));
+    vaciarCarrito.addEventListener('click', ()=>{
+        carrito=[];
+        limpiarCarrito()
+    })
+};
+
+function agregarCurso(e){
+    e.preventDefault();
+    obtenerInfoCurso(e.target.parentElement.parentElement);
+}
+
+function obtenerInfoCurso(card){
+    
+    const info = {
+        img: card.querySelector('img').src,
+        titulo: card.querySelector('h4').textContent,
+        precio: card.querySelector('p span').textContent,
+        id: card.querySelector('a').dataset.id,
+        cantidad: 1
+    };
+
+    const duplicado = carrito.some(curso => curso.id === info.id);
+
+    if(duplicado){
+        const cursos = carrito.map(curso =>{
+            if(curso.id === info.id){
+                curso.cantidad++;
+                return curso;
+            } else {
+                return curso;
+            };
+        });
+    } else {
+        carrito.push(info);
+    }
+    
+    mostrarEnCarrito()
+}
+
+function mostrarEnCarrito() {
+    
+    limpiarCarrito();
+    
+    carrito.forEach(curso=>{
+        
+        const row = document.createElement('tr');
+        const { img , titulo , precio , cantidad , id } = curso;
+        row.innerHTML =`
+            <td>
+                <img src="${img}">
+            </td>
+            <td>
+                ${titulo}
+            </td>
+            <td>
+                ${precio}
+            </td>
+            <td>
+                ${cantidad}
+            </td>
+            <td>
+                <a href="#" class="borrar-curso" onclick="eliminarCurso(this)" data-id="${id}"> X </a>
+            </td>`;
+
+        listaCarrito.appendChild(row);
+    
+    })
+    
+};
+
+function limpiarCarrito(){
+    while(listaCarrito.firstChild){
+        listaCarrito.removeChild(listaCarrito.firstChild)
+    }
+}
+
+function eliminarCurso(e){
+    carrito = carrito.filter(curso => curso.id !== e.dataset.id)
+    mostrarEnCarrito()
+}
